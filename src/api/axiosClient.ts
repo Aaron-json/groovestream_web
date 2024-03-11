f2c833d3598a1dc093b5b4c134c7c6b22531431e
Aaron-json <aronmramba@gmail.com>
2024-03-11 02:57:38 -0600

    Revert "Fix env variables not working in prod."
    This reverts commit 6247d256deb1c14fe3d42565cc328c57e6bc48d3.

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
-const API_URL = "https://fine-craft-385619.nn.r.appspot.com"
-const DEV_API_URL = "http://localhost:5001"
+
 const axiosClient = axios.create({
-  baseURL: import.meta.env.PROD ? API_URL : DEV_API_URL,
+  baseURL: import.meta.env.PROD ? import.meta.env.VITE_API_URL : import.meta.env.VITE_DEV_API_URL,
 });
 // timeout for all requests. Independent requests can set their own timeouts
 // if they take longer. ex - media upload
