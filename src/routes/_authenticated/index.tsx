6958935959e01a0db37c1b5ab517e6eb4943a36d
Aaron <aronmramba@gmail.com>
2025-03-28 14:50:56 -0600

    use info card on home page

--- src/routes/_authenticated/index.tsx
+++ src/routes/_authenticated/index.tsx
@@ -1,8 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useMostPlayed, useListeningHistory } from "@/hooks/media";
 import MediaList from "@/components/custom/media-list";
-import { Card, CardContent } from "@/components/ui/card";
-import { Music2 } from "lucide-react";
 import InfoCard from "@/components/custom/info-card";
 import { getUser } from "@/api/requests/user";
 import { useQuery } from "@tanstack/react-query";
@@ -44,14 +42,7 @@ function RouteComponent() {
     !history ||
     !user
   ) {
-    return (
-      <Card className="max-w-md mx-auto mt-8">
-        <CardContent className="flex items-center justify-center p-8 text-muted-foreground">
-          <Music2 className="w-6 h-6 mr-2" />
-          <span>Error encountered</span>
-        </CardContent>
-      </Card>
-    );
+    return <InfoCard text="Error encountered" />;
   } else if (mostPlayed.length === 0 && history.length === 0) {
     return (
       <InfoCard
