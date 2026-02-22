134d66b21867067f37bbf0349d2a429e681b773d
Aaron-json <90925260+Aaron-json@users.noreply.github.com>
2026-02-22 16:02:04 -0700

    fix auth file interceptors and remove usecallback

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import { useRef, useCallback } from "react";
+import { useRef } from "react";
 import axiosClient from "../api/axiosClient";
 import { Session } from "@supabase/supabase-js";
 import { useState, useEffect } from "react";
@@ -8,6 +8,26 @@ import { Route as AuthRoute } from "@/routes/auth";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const anon_key = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabaseClient = createClient(supabaseUrl, anon_key);
+
+// Returns an authentication token. If the user is not authenticated, throws an error.
+async function getAuthToken(): Promise<string | undefined> {
+  const curSession = await supabaseClient.auth.getSession();
+
+  if (curSession.error) {
+    throw curSession.error;
+  } else if (!curSession.data.session) {
+    throw new Error("No session");
+  } else {
+    return curSession.data.session.access_token;
+  }
+}
+
+// attack auth interceptor to axiosClient for
+axiosClient.interceptors.request.use(async (config) => {
+  const token = await getAuthToken();
+  config.headers.Authorization = "Bearer " + token;
+  return config;
+});
 
 export async function signInGoogle() {
   const location = `${window.location.origin}/${AuthRoute.path}`;
@@ -38,40 +58,20 @@ export function useAuth() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>();
   const sessionRef = useRef<Session | null>(null);
 
-  const onSessionChange = useCallback((session: Session | null) => {
-    sessionRef.current = session;
-    setIsAuthenticated(!!session);
-  }, []);
   useEffect(() => {
     // get the initial session
     const { data } = supabaseClient.auth.onAuthStateChange((_, session) => {
-      onSessionChange(session);
+      sessionRef.current = session;
+      setIsAuthenticated(!!session);
     });
 
     // get initial session. will trigger onAuthStateChange
     supabaseClient.auth.getSession();
 
-    // set interceptor to add auth header to requests
-    axiosClient.interceptors.request.use(async (config) => {
-      const token = await checkRequestAuth();
-      config.headers.Authorization = "Bearer " + token;
-      return config;
-    });
-    return () => data.subscription.unsubscribe();
+    return () => {
+      data.subscription.unsubscribe();
+    };
   }, []);
 
   return { sessionRef, isAuthenticated } as const;
-}
-
-// Returns an authentication token. If the user is not authenticated, throws an error.
-export async function checkRequestAuth(): Promise<string | undefined> {
-  const curSession = await supabaseClient.auth.getSession();
-
-  if (curSession.error) {
-    throw curSession.error;
-  } else if (!curSession.data.session) {
-    throw new Error("No session");
-  } else {
-    return curSession.data.session.access_token;
-  }
 }
