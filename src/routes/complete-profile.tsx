3ca1e951a70e1ac2462702e5610152f135727f26
Aaron-json <90925260+Aaron-json@users.noreply.github.com>
2026-02-22 12:02:57 -0700

    fix text logo import

--- src/routes/complete-profile.tsx
+++ src/routes/complete-profile.tsx
@@ -6,7 +6,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { CheckCircle, AlertCircle } from "lucide-react";
-import TextLogo from "@/components/custom/textlogo";
+import { TextLogo } from "@/components/custom/textlogo";
 import { createUserProfile, usernameExists } from "@/api/requests/user";
 import { queryClient } from "@/lib/query";
 
