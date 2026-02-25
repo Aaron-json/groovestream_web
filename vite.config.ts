f9286ae25e598c64336acd59ffb2515e15f0ead5
Aaron-json <90925260+Aaron-json@users.noreply.github.com>
2026-02-24 19:07:32 -0700

    build tanstack query into separate chunk for better chunking

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig, type PluginOption } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { visualizer } from "rollup-plugin-visualizer";
+// import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,20 +14,24 @@ export default defineConfig({
     }),
     react(),
     tailwindcss(),
-    visualizer({
-      filename: ".build/bundle-stats.md",
-      template: "markdown",
-      sourcemap: true,
-      gzipSize: true,
-      brotliSize: true,
-    }) as PluginOption,
+    // accorinng to docs, keep this last if sourcemaps are used
+    // uncomoment to generate bundle stats after build
+    // visualizer({
+    //   filename: ".build/bundle-stats.html",
+    //   template: "flamegraph",
+    //   sourcemap: true,
+    //   gzipSize: true,
+    //   brotliSize: true,
+    // }),
   ],
   build: {
+    // helps visualizer to have more accurate sizes
+    sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: {
-          // shaka does not benefit much from tree-shaking.
           "shaka-player": ["shaka-player"],
+          "tanstack-query": ["@tanstack/react-query"],
         },
       },
     },
