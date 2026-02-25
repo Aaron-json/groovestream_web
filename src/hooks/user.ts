e75fe74fbe97e5125ed703b09218b090c024ec0d
Aaron-json <90925260+Aaron-json@users.noreply.github.com>
2026-02-24 18:17:32 -0700

    userKey refactor

--- src/hooks/user.ts
+++ src/hooks/user.ts
@@ -1,17 +1,14 @@
 import { getUser } from "@/api/requests/user";
 import { useQuery } from "@tanstack/react-query";
 
-export function getUserKey() {
-  return ["user"];
-}
+export const userKey = ["user"] as const;
 
 export function useUser() {
-  const queryKey = getUserKey();
   const query = useQuery({
-    queryKey,
+    queryKey: userKey,
     queryFn: getUser,
     // user data does not change frequently
     staleTime: 1000 * 60 * 15, // an hour
   });
-  return { ...query, queryKey } as const;
+  return { ...query, queryKey: userKey } as const;
 }
