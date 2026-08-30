abef67ce3884b739aeef8ff9c30d2ee31dfffff0
Aaron-json <90925260+Aaron-json@users.noreply.github.com>
2026-08-30 16:05:40 +0300

    refetch on focus/reconnect

--- apps/web/src/lib/query.ts
+++ apps/web/src/lib/query.ts
@@ -3,8 +3,8 @@ export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: false,
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
+      refetchOnWindowFocus: true,
+      refetchOnReconnect: true,
       // Low enough to catch quick UI changes but not high enough to cause
       // meaningful data delay/lag
       // If more fine grained control is needed, queries should manually
